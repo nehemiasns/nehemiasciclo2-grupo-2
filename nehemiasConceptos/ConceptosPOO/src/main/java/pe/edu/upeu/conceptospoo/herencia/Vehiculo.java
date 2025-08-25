c533d57d374460e5bf87d8c6627075732e69f547
nehemiasml <118933540+nehemiasns@users.noreply.github.com>
2025-08-25 09:30:01 -0500

    Add files via upload

---
+++ nehemiasConceptos/ConceptosPOO/src/main/java/pe/edu/upeu/conceptospoo/herencia/Vehiculo.java
@@ -0,0 +1,10 @@
+package pe.edu.upeu.conceptospoo.herencia;
+
+public class Vehiculo {
+    protected String marca="Ford";
+
+    public String sonido() {
+        return "Tuut...tuut!...";
+    }
+
+}
